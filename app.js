2385bc72e59942b2b82ac7f55e30540c84b80203
Peter <peter.m.farmer.204@gmail.com>
2023-04-24 00:17:02 +1000

    added cells through JS and added basic unfinished functionality for player to interact with cells

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+function createCells(tableSize) {
+  const tableBody = document.getElementById("table-body");
+  for (let i = 0; i < tableSize; i++) {
+    let tableRow = document.createElement("tr");
+
+    for (let j = 0; j < tableSize; j++) {
+      let tableCell = document.createElement("td");
+      tableCell.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        console.log(`${e.button} (${e.type})`);
+        //TODO: get left click functionality working
+        if (e.button === 1) {
+          console.log("success");
+          tableCell.classList.add("shown");
+        } else if (e.button === 2) {
+          tableCell.classList.add("flag");
+        }
+      });
+
+      tableRow.appendChild(tableCell);
+    }
+
+    tableBody.appendChild(tableRow);
+  }
+}
+
+createCells(6);
